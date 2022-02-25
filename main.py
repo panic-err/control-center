d24ff1546189b77d357452965d85f8531fe94c7b
Ben Belland <benbelland@gmail.com>
2022-02-25 03:13:18 -0800

    doot doot!

--- main.py
+++ main.py
@@ -5,18 +5,21 @@ import matplotlib.pyplot as plt
 
 
 class Detector(object):
-    lbpCascade = cv2.CascadeClassifier('cascades/lbp0.xml')
+    lbpCascade = cv2.CascadeClassifier('cascades/doot.xml')
+
+    #lbpCascade = cv2.CascadeClassifier('cascades/lbp0.xml')
     haarCascade = cv2.CascadeClassifier('cascades/haar.xml')
     
     def lbp(self, img):
         toDetect = img.copy()
         
-        hornRect = self.lbpCascade.detectMultiScale(toDetect, scaleFactor = 1.2, minNeighbors = 5)
+        toDet = cv2.cvtColor(toDetect, cv2.COLOR_BGR2GRAY)
+        hornRect = self.lbpCascade.detectMultiScale(toDet, scaleFactor = 1.1, minNeighbors = 175)
         
         for (x, y, w, h) in hornRect:
-            cv2.rectangle(toDetect, (x, y), (x+w, y +h), (10, 10, 200), 10)
+            cv2.rectangle(toDet, (x, y), (x+w, y +h), (10, 10, 200), 10)
                           
-        return toDetect
+        return toDet
 
     def haar(self, img):
         toDetect = img.copy()
@@ -37,13 +40,13 @@ while(True):
     ret, frame = vido.read()
     
     #detect using lbp
-    #toUseLBP = frame.copy()
-    #im = det.lbp(toUseLBP)
+    toUseLBP = frame.copy()
+    im = det.lbp(toUseLBP)
     
     
     #detect using haar
-    toUseHaar = frame.copy()
-    im = det.haar(toUseHaar)
+    #toUseHaar = frame.copy()
+    #im = det.haar(toUseHaar)
     
     #Show the image
     cv2.imshow('lbp vs haar', im)
