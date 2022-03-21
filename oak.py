704f88f0472cda8b0c25e4ef4c16ad8d1ffc53b1
Ben Belland <benbelland@gmail.com>
2022-03-21 15:30:48 -0700

    stable checkpoint

--- oak.py
+++ oak.py
@@ -1,31 +1,43 @@
 #!/~/bin/python
 
 import depthai as dai
+import cv2
 
 pipeline = dai.Pipeline()
-
-xIn = pipeline.create(dai.node.XLinkIn)
-xOut = pipeline.create(dai.node.XLinkOut)
-xOut.setStreamName("GetFrom")
-xIn.setStreamName("h264orbust")
 cam = pipeline.create(dai.node.ColorCamera)
-cam.setPreviewSize(300, 300)
+PreviewSize = (300, 300)
+
+XLinkIn = pipeline.create(dai.node.XLinkIn)
+XLinkIn.setStreamName("input_stream")
+
+cam.setPreviewSize(PreviewSize)
 cam.setBoardSocket(dai.CameraBoardSocket.RGB)
-cam.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
+cam.setResolution(dai.ColorCameraProperties.SensorResolution.THE_13_MP)
 cam.setInterleaved(False)
 cam.setColorOrder(dai.ColorCameraProperties.ColorOrder.RGB)
 
+XLinkIn.out.link(cam.inputControl)
 
-xIn.out.link(cam.inputControl)
+#cam.link(cam.inputControl)
+
+
+
+XLinkOut = pipeline.create(dai.node.XLinkOut)
+XLinkOut.setStreamName("output_stream")
+
+
+#In.out.link(cam.inputControl)
 #xOut.link(cam.still)
-with dai.Device(pipeline) as device:
-    device.startPipeline()
+with dai.Device() as device:
+    device.startPipeline(pipeline)
     print('MxId:', device.getDeviceInfo().getMxId())
     print('USB Speed:', device.getUsbSpeed())
     print('Connected Cameras:', device.getConnectedCameras())
     
-    input_q = device.getInputQueue("h264orbust")
-    output_q = device.getOutputQueue("GetFrom", maxSize=1, blocking=False)
+    input_q = device.getInputQueue("input_stream")
+    output_q = device.getOutputQueue("output_stream", maxSize=1, blocking=False)
+    frame = None
+    
     
     ctrl = dai.CameraControl()
     
@@ -35,7 +47,25 @@ with dai.Device(pipeline) as device:
     #input_q = device.getInputQueue("input_name")
     #output_q = device.getOutputQueue("output_name", maxSize=1, blocking=False)
     count = 0
+    fps = cam.getFps()
+    print("Current FPS is " + str(fps))
+    
+    #pic = output_q.tryGet()
+    #print(pic)
     while True:
-        fps = cam.getFps()
-        print("FPS: " + str(fps))
+        inRGB = output_q.tryGet()
+    
+        if inRGB is not None:
+            #Currently this doesn't trigger the input queue, but I've only attempted
+            #to take a photo once and I probably got it wrong. to the queue!
+            frame = inRGB.getCvFrame()
+            cv2.imshow("RGB camera stills 12 MP", frame)
+    
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    cv2.destroyAllWindows()
+    
+    #while True:
+    #    fps = cam.getFps()
+    #    print("FPS: " + str(fps))
     
